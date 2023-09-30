4604edb23befe36b78175d1da750c13e1243a830
nedacr <robertsoncaden02@gmail.com>
2023-09-30 01:28:43 -0400

    new list format- updated Front End

---
+++ src/userTable.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Table = () => {
+  return (
+    <table>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Age</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>John</td>
+          <td>30</td>
+        </tr>
+        <tr>
+          <td>Jane</td>
+          <td>25</td>
+        </tr>
+      </tbody>
+    </table>
+  );
+};
+
+export default Table;
